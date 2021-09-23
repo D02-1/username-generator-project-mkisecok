b84749c91cf632200ee9106c689def62ebaf607a
Mkisecok <mahmutkisecok@gmail.com>
2021-09-23 11:02:58 +0200

    Add createRandomNumber(x,y;function

--- src/index.js
+++ src/index.js
@@ -14,12 +14,32 @@ const path = require('path'); // Path, zum areiten mit dateipfaden
  */
 function getWords()
 {
+    //Wir lesen eine json datei, die worte enhält, die wir nutzen wollen
     const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), 'utf-8'));
-   return json;
+   
+   //Wir geben die worte zurück
+    return json;
 
 }
 getWords()
 // - Eine Funktion um die uufällige Nummer hinter dem Namen zu generieren
+
+/**
+ * @function createRandomNumber
+ * @description Creates a random number according to a minimal and maximal value
+ * @param {number} minNumber 
+ * @param {number} maxNumber 
+ * @returns {number}
+ */
+function createRandomNumber(minNumber, maxNumber)
+{
+    // Wir erscaffen eine zufallszahl aus einer minimalen und einer maximalen zahl
+    const num =Math.floor(Math.random()* (maxNumber -minNumber) + minNumber);
+
+         //Wir geben die zufällige nummer zurück
+    return num;
+}
+
 // - Eine Funktion mit der wir den ersten Buchstaben des Jeweiligen Wortes groß schreiben (PascalCase)
 // - Eine Funktion mit der wir den benutzernahmen generieren und auf die ergebnisse der anderen funktionen zugreifen
 // - Einen export, um die Applikation in einer anderen datei zu nutzen
